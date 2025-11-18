820d49b8c1cae31a0ced394865a78a6b1e6a0510
Kenichi Maeda <kenichi.maeda121@gmail.com>
2025-11-18 00:21:47 -0500

    Change training method

--- trainer.py
+++ trainer.py
@@ -12,22 +12,80 @@ from src.data.roadgraph_dm import RoadGraphDataModule
 
 
 def main():
+    seed_everything(42) 
+    
+    # Stage 1
+    # dm = RoadGraphDataModule(
+    #     root="/oscar/home/kmaeda2/RoadGraphPlus",
+    #     batch_size=32,
+    #     max_items=None
+    # )
 
+    # wandb_logger = WandbLogger(
+    #     project="roadgraph",
+    #     name="exp_step1",
+    #     log_model=False
+    # )
+
+    # model = BaselineModel(lr=1e-3, 
+    #                       weight_decay=1e-5, 
+    #                       warmup_epochs=50,
+    #                       anneal_epochs=1000,
+    #                       lambda_e=0.0)
+    # checkpoint_cb = ModelCheckpoint(
+    #     dirpath="checkpoints_stage1/",
+    #     save_last=True,
+    #     save_top_k=0
+    # )
+    # trainer = Trainer(
+    #     logger=wandb_logger,
+    #     enable_checkpointing=True,
+    #     callbacks=[checkpoint_cb],
+    #     enable_progress_bar=True,
+    #     max_epochs=50,
+    #     accelerator="auto",
+    #     log_every_n_steps=1,
+    #     strategy="ddp_find_unused_parameters_true",
+    # )
+    # trainer.fit(model, datamodule=dm)
+
+    # Stage 2
     dm = RoadGraphDataModule(
         root="/oscar/home/kmaeda2/RoadGraphPlus",
-        max_items=500,
-        batch_size=16
+        batch_size=32,
+        max_items=None
     )
 
-    model = BaselineModel(lr=1e-4, weight_decay=1e-5)
+    wandb_logger = WandbLogger(
+        project="roadgraph",
+        name="exp_step2",
+        log_model=False
+    )
+
+    model = BaselineModel.load_from_checkpoint(
+                          "checkpoints_stage1/last.ckpt",
+                          lr=1e-3, 
+                          weight_decay=1e-5, 
+                          warmup_epochs=10,
+                          anneal_epochs=20,
+                          min_gt_prob=0.2)
+    
+    checkpoint_cb = ModelCheckpoint(
+        dirpath="checkpoints_stage2/",
+        save_last=True,
+        save_top_k=0
+    )
 
     trainer = Trainer(
-        enable_checkpointing=False,
+        logger=wandb_logger,
+        enable_checkpointing=True,
+        callbacks=[checkpoint_cb],
         enable_progress_bar=True,
         max_epochs=50,
         accelerator="auto",
+        log_every_n_steps=1,
+        strategy="ddp_find_unused_parameters_true",
     )
-
     trainer.fit(model, datamodule=dm)
 
 
