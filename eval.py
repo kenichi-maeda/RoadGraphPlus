bec5319f1752003fa66e2c1eef14a93d513eb52f
Kenichi Maeda <kenichi.maeda121@gmail.com>
2025-11-18 00:22:46 -0500

    Implement eval

---
+++ eval.py
@@ -0,0 +1,44 @@
+import importlib
+import os
+import random
+import time
+
+from lightning.pytorch import Trainer, seed_everything
+from lightning.pytorch.callbacks import ModelCheckpoint
+from lightning.pytorch.loggers import WandbLogger
+
+from src.models.baseline_model import BaselineModel
+from src.data.roadgraph_dm import RoadGraphDataModule
+
+
+def main():
+    seed_everything(42) 
+
+    dm = RoadGraphDataModule(
+        root="/oscar/home/kmaeda2/RoadGraphPlus",
+        batch_size=32,
+        max_items=None
+    )
+
+    # Step 3: Evaluation
+    model = BaselineModel.load_from_checkpoint(
+        "checkpoints_stage2/last.ckpt",
+        warmup_epochs=10,
+        anneal_epochs=20,
+        min_gt_prob=0.2,
+        lr=1e-3,
+        weight_decay=1e-5
+    )
+
+
+    trainer = Trainer(
+        accelerator="auto",
+        log_every_n_steps=1,
+        strategy="ddp_find_unused_parameters_true",
+    )
+
+    trainer.test(model, datamodule=dm)
+
+
+if __name__ == "__main__":
+    main()
