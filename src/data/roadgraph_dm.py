27169fbbb990004421d2a899654fe87d7813102f
Kenichi Maeda <kenichi.maeda121@gmail.com>
2025-11-18 00:23:50 -0500

    Make val & test dataloader

--- src/data/roadgraph_dm.py
+++ src/data/roadgraph_dm.py
@@ -2,23 +2,59 @@ import lightning.pytorch as pl
 from torch.utils.data import DataLoader
 from .roadgraph_ds import RoadGraphDataset
 from .collate import collate
+from pathlib import Path
 
 class RoadGraphDataModule(pl.LightningDataModule):
-    def __init__(self, root, max_items=20, batch_size=4):
+    def __init__(self, root, max_items=20, batch_size=4, val_frac=0.1, test_frac=0.1):
         super().__init__()
-        self.root = root
+        self.root = Path(root)
         self.max_items = max_items
         self.batch_size = batch_size
+        self.val_frac = val_frac
+        self.test_frac = test_frac
 
     def setup(self, stage=None):
-        self.ds = RoadGraphDataset(self.root, max_items=self.max_items)
+        all_files = sorted((self.root / "data/labels").glob("*.json"))
+
+        if self.max_items is not None:
+            all_files = all_files[:self.max_items]
+
+        N = len(all_files)
+        Nv = int(N * self.val_frac)
+        Nt = int(N * self.test_frac)
+
+        self.test_files = all_files[:Nt]
+        self.val_files = all_files[Nt: Nt + Nv]
+        self.train_files = all_files[Nt + Nv:]
+
+        self.train_ds = RoadGraphDataset(self.train_files)
+        self.val_ds = RoadGraphDataset(self.val_files)
+        self.test_ds = RoadGraphDataset(self.test_files)
 
 
     def train_dataloader(self):
         return DataLoader(
-            self.ds,
+            self.train_ds,
             batch_size=self.batch_size,
             shuffle=True,
-            num_workers=0,
+            num_workers=4,
+            collate_fn=collate
+        )
+    
+    def val_dataloader(self):
+        return DataLoader(
+            self.val_ds,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=4,
+            collate_fn=collate
+        )
+    
+    def test_dataloader(self):
+        return DataLoader(
+            self.test_ds,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=4,
             collate_fn=collate
         )
